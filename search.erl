caf8c265b3b7ab7dc838c039ac381044a686fa58
Daniel <dcortes92@hotmail.com>
2013-08-25 14:24:22 -0600

    Agregado board.erl y search.erl

---
+++ search.erl
@@ -0,0 +1,16 @@
+% Instituto Tecnológico de Costa Rica
+% Escuela de Ingeniería en Computación
+% Inteligencia Artificial
+% I Tarea Programada 
+% Daniel Cortés Sáenz, 201120152
+% Descripción: implementación de algorítmos de búsqueda de ruta óptima informados:
+%				1. Greedy:
+%				2. A*:
+%				3. 
+
+-module(search).
+
+-author('dcortes92@hotmail.com').
+-export([main/0]).
+
+main() -> board ! {get_pos, self()}, receive X -> X end.
