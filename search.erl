a1ffdee83eb53147d701d838adb8eaf0d9535fab
Daniel <dcortes92@hotmail.com>
2013-08-26 00:34:09 -0600

    Implementado agoritmo greedy

--- search.erl
+++ search.erl
@@ -11,7 +11,7 @@
 -module(search).
 -author('dcortes92@hotmail.com').
 -import(board). %para utilizar las mismas funciones que se usan en board
--export([greedy/0, min/1, greedy_algorithm/2, test/1]).
+-export([greedy/0, min/1, greedy_algorithm/2]).
 
 %Obtiene el indice del menor de la lista junto con su indice
 min([]) -> error;
@@ -21,8 +21,6 @@ min([H|T]) -> min_aux(T, H, 1, 2).
 min_aux([], Min, IndiceMin, _) -> {Min, IndiceMin};
 min_aux([T|C], Min, _, IndiceActual) when(T < Min) -> min_aux(C, T, IndiceActual, IndiceActual+1);
 min_aux([_|C], Min, IndiceMin, IndiceActual) -> min_aux(C, Min, IndiceMin, IndiceActual+1).
-
-test([_|T]) -> T.
 
 %Algoritmo greedy best-first.
 greedy() -> 
@@ -41,15 +39,15 @@ greedy_algorithm({X, Y}, {Fringe, Heuristica}) ->
 		{_, _} -> 
 			board ! {get_neighbors, self()}, %En otro caso se obtienen los vecinos y se continua con el algoritmo
 			receive 
-				{[], _} -> 
-					if Fringe == [] -> 
+				{[], _} -> %Si ya no hay vecinos
+					if Fringe == [] -> %Si no hay vecinos y el fringe está vacío
 						io:format("No se ha encontrado una ruta."), 
 						fail; %En caso de que no se encuentre una ruta
 					true -> %Else si ya no hay vecinos pero queda algo en el fringe
 						{Minimo, Indice} = min(Heuristica),
 						Siguiente = lists:nth(Indice, Fringe),
-						board ! {move, Siguiente},
-						greedy_algorithm({X, Y},
+						board ! {move, Siguiente}, 
+						greedy_algorithm({X, Y}, %Se devuelve
                                     {lists:delete(Siguiente, Fringe),
                                      lists:delete(Minimo, Heuristica)})
 					end;
