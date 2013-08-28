bba16cf8836f136d1bfd1a7ea143ae1d7bb82433
Daniel <dcortes92@hotmail.com>
2013-08-28 00:37:22 -0600

    Implementado A* en linea recta

--- search.erl
+++ search.erl
@@ -11,7 +11,8 @@
 -module(search).
 -author('dcortes92@hotmail.com').
 -import(board). %para utilizar las mismas funciones que se usan en board
--export([greedy/0, min/1, greedy_algorithm/2, esta/2, g/2, euclides/2, agregarOpen/3]).
+-export([greedy/0, min/1, greedy_algorithm/2, g/2, euclides/2, astar/0, astar_algorithm/4, 
+		agregarOpen/4, menor_f/1, esta/2]).
 
 %Obtiene el indice del menor de la lista junto con su indice
 min([]) -> error;
@@ -21,6 +22,23 @@ min([H|T]) -> min_aux(T, H, 1, 2).
 min_aux([], Min, IndiceMin, _) -> {Min, IndiceMin};
 min_aux([T|C], Min, _, IndiceActual) when(T < Min) -> min_aux(C, T, IndiceActual, IndiceActual+1);
 min_aux([_|C], Min, IndiceMin, IndiceActual) -> min_aux(C, Min, IndiceMin, IndiceActual+1).
+
+%Obtiene el cuadro con el menor F de la lista y su posición en esta lista (para luego borrarlo)
+menor_f([]) -> [];
+menor_f([H]) -> {H,1}; %Si solo hay un elemento retorne ese punto X,Y
+menor_f([H|T]) -> menor_f_aux(T, H, 1, 2).
+
+menor_f_aux([], Menor, IndiceMin, _) -> {Menor, IndiceMin};
+menor_f_aux([T|C], Menor, IndiceMin, IndiceActual) -> 
+	FT = calcular_f(lists:nth(2, T), lists:nth(3, T)),
+	FMen = calcular_f(lists:nth(2, Menor), lists:nth(3, Menor)),
+	if(FT < FMen) ->
+		menor_f_aux(C, T, IndiceActual, IndiceActual+1);
+	true ->
+		menor_f_aux(C, Menor, IndiceMin, IndiceActual+1)
+	end.
+
+calcular_f(G, H) -> G + H.
 
 %Algoritmo greedy best-first.
 greedy() -> 
@@ -72,27 +90,64 @@ astar() ->
 			board ! {get_pos, self()},
 			receive										 %Lista open
 				{A, B} -> spawn(search, astar_algorithm, [[{A, B}, 0, euclides({A,B}, {X,Y})],
-														 %Lista Closed
-														 [],
 														 %Actual
 														 [{A, B}, 0, euclides({A,B}, {X,Y})],
 														 %Objetivo
-														 {X,Y}])
+														 {X,Y},
+														 %Lista de padres
+														 []])
 			end
 	end.
 
-
-astar_algorithm(Open, Closed, Actual, {X, Y}) -> ok.
+%Logica del algoritmo astar
+astar_algorithm(Open, Actual, {X, Y}, Padres) -> 
+	board ! {get_pos, self()},
+	receive
+		{I, J} when (I == X) and (J == Y) -> %En caso de que estemos en el objetivo
+			io:format("Encontrado, calculando ruta...~n"),
+			epicwin;
+		{_,_} ->
+			%Buscar el cuadro con el menor F en la lista abierta
+			board ! {get_neighbors, self()},
+			receive 
+				{[], _} -> %Si ya no hay vecinos
+					if Open == [] -> %Si no hay vecinos y el fringe está vacío
+						io:format("No se ha encontrado una ruta."), 
+						fail; %En caso de que no se encuentre una ruta
+					true -> %Else si ya no hay vecinos pero queda algo en el fringe
+						%{Minimo, Indice} = menor_f(Open),
+						%Siguiente = lists:nth(Indice, Open),
+						%board ! {move, lists:nth(1, Siguiente)}, 
+						%NuevoOpen = lists:delete(Siguiente, Open), %Se quita el siguiente del fringe con su heuristica
+						%astar_algorithm(NuevoOpen, Actual, {X, Y}, Padres) %Para devolverse
+						io:format("Open es: ", Open)
+					end;
+				{Celdas, _} -> %En otro caso, se mueve a la mejor celda y se quita esa
+										 %celda de las celdas visitadas.
+					OpenTemp = agregarOpen( Celdas, {X,Y}, lists:nth(2, Actual), lists:nth(1, Actual) ),
+                    {Minimo, Indice} = menor_f(OpenTemp),
+                    Siguiente = lists:nth(Indice, OpenTemp),
+                    board ! {move, lists:nth(1,Siguiente)},
+                    NuevoOpen = lists:delete(Siguiente, OpenTemp),
+					%Se agregan a las heuristicas y los vecinos al frige a las estructuras ya creadas
+					%excepto el del campo escodgido.
+                    astar_algorithm(lists:append(Open, NuevoOpen), Actual, {X, Y}, Padres) 
+			end
+	end.
 
 
 
 %Para saber si un punto x,y está en una lista. Se usa en el A* para saber
 %si un nodo está o no en alguna de las listas closed u opened.
-esta({_,_}, []) -> [];
-esta({X, Y}, [{X, Y}|_]) -> yes;
-esta({X, Y}, [_|T]) -> esta({X,Y}, T).
-
-
+esta(_, []) -> 0;
+esta({X,Y}, [H|T]) ->
+	{I,J} = lists:nth(1, H),
+	if (I == X) and (J == Y)->
+		1;
+	true->
+		esta({X,Y}, T)
+	end.
+ 
 %Costo de ir de un punto X,Y a otro punto W,Z
 g({X, Y}, {W, Z}) when (X /= W) and (Y /= Z) -> 2;
 g(_,_) -> 1.
@@ -103,11 +158,11 @@ euclides({X, Y}, {W, Z}) -> math:sqrt(math:pow(Z-Y, 2) + math:pow(W-X, 2)).
 
 
 %Agrega cada elemento al open de la forma [[{X,Y}, G({X,Y}), H({X,Y})]]
-agregarOpen([H|T], {X,Y}, Actual) ->
+agregarOpen([H|T], {X,Y}, Actual, {A1,A2}) ->
 [
 	[H, B, C] || 
-		B <- [Actual + g(H, {X, Y})],
+		B <- [Actual + g(H, {A1, A2})],
 		C <- [euclides(H, {X,Y})]
-] ++ agregarOpen(T, {X,Y}, Actual);
+] ++ agregarOpen(T, {X,Y}, Actual, {A1,A2});
 
-agregarOpen([], _, _) -> [].
+agregarOpen([], _, _, _) -> [].
