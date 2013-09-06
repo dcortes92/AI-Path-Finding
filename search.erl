3ef2d3819b6f2e35ed61e9d77c34abe165a54cbc
Daniel <dcortes92@hotmail.com>
2013-09-05 19:56:06 -0600

    Correccion de errores Jumping Points

--- search.erl
+++ search.erl
@@ -18,8 +18,7 @@
 -author('dcortes92@hotmail.com').
 -import(board). %para utilizar las mismas funciones que se usan en board
 -export([greedy/0, min/1, greedy_algorithm/2, euclides/2, astar/0, astar_algorithm/3, 
-		astar_algorithm_proc_vecinos/5, agregarOpen/3, menor_f/1, esta/3]).
--compile(export_all).
+		astar_algorithm_proc_vecinos/5, agregarOpen/3, menor_f/1, esta/3, jp/0]).
 -define(WALL, 1).
 
 
@@ -325,6 +324,16 @@ jp_reconstruir_ruta([H | T], Retorno, LN) ->
 	true ->
 			jp_reconstruir_ruta(T, Retorno, LN)
 	end.
+
+
+estaPadre(_, []) -> false;
+estaPadre(Coord, [H | T]) ->
+	Temp = lists:nth(6, H),
+	if Temp == Coord ->
+		true;
+	true->
+		estaPadre(Coord, T)
+	end.
 						
 %Imprime la ruta en el board y en consola pra ver los padres de cada nodo
 mostrarRuta([]) -> ok ;
@@ -336,10 +345,10 @@ mostrarRuta([Nodo | Resto]) ->
 %Procesar la lista open
 procesarOpen([] , Global, _, _) -> [[], Global];
 procesarOpen(Open, Global, Final, NodosVisitados) ->
-	Menor = obtenerMinimo(Open),
+	Menor = menorFJP(Open),
 	NuevaOpen = lists:delete(Menor, Open),
 	Coordenadas = lists:nth(1, Menor),
-	NuevaGlobal = closeNodo(Coordenadas, Global, []),
+	NuevaGlobal = cerrarNodo(Coordenadas, Global),
 	if Coordenadas == Final -> %Se ha llegado al objetivo
 			[NuevaOpen, NuevaGlobal];
 	true -> 
@@ -350,6 +359,42 @@ procesarOpen(Open, Global, Final, NodosVisitados) ->
 				lists:nth(2, PosSucesores), Final, 
 				lists:nth(3, PosSucesores))
 	end.
+
+%Nodo con el menor F de la lista Open, similar al A*
+menorFJP([]) -> [];
+menorFJP(Open = [H | T]) -> 
+	Fs = calcularFNodos([H | T]),
+	Minimo = lists:min(Fs),
+	I = indiceMenorF(Fs, Minimo, 1),
+	lists:nth(I, Open).
+
+
+%Calcula el Fs de los nodos en una lista
+calcularFNodos([]) -> [];
+calcularFNodos([H | T]) ->
+	F = calcular_f(lists:nth(2, H) , lists:nth(3, H)),
+	[F] ++ calcularFNodos(T).	
+
+
+%Retorna el índice de un nodo en la lista
+indiceMenorF([H|_], Min, I) when H == Min -> I;
+indiceMenorF([_|T], Min, I) -> 
+	IS = I + 1,
+	indiceMenorF(T, Min, IS).
+
+%Se marca un nodo como cerrado en la lista global
+cerrarNodo(_, []) -> [];
+cerrarNodo(Nodo, [H | T]) -> 
+	Temporal = lists:nth(1, H),
+	NodoG = lists:nth(2, H),
+	NodoH = lists:nth(3, H),
+	NodoListaOpen = lists:nth(4, H),
+	Padre = lists:nth(6 , H),
+	if Nodo /= Temporal -> 
+		[H] ++ cerrarNodo(Nodo, T);
+	true ->
+		[[Nodo, NodoG, NodoH, NodoListaOpen, 1, Padre]]
+	end.	
 
 
 %Para identificar los posibles sucesores de un vecino
@@ -363,7 +408,7 @@ posiblesSucesores(Menor, Open, Global, {Fx, Fy}, NodosVisitados) ->
 					[Open, Global, NodosVisitados];
 			true ->
 				%Retorna la nuevas listas con las que se debe seguir trabajando
-				foreachVecino(Neighbors, Global, Menor, Open, {Fx, Fy}, NodosVisitados)	
+				jp_algorithm_proc_vecinos(Neighbors, Global, Menor, Open, {Fx, Fy}, NodosVisitados)	
 			end
 	end.
 
@@ -377,191 +422,56 @@ nuevosVecinos([H | T], Visitados) ->
 			[H] ++ nuevosVecinos(T, Visitados)
 	end.
 
-%Recorre la lista de vecinos para irse expandiendo.	
-foreachVecino([H | T], ListaGlobal, Nodo = [{X, Y}, G , _], OpenList, {Fx, Fy}, Visitados) -> 
-	Vecino = H,
-	board ! {movejp, H},
-	VisitadosN = lists:append(Visitados, [H]),						
-	JumpPoint = jump(Vecino, {X, Y}, {Fx, Fy}),
-	if JumpPoint /= [] ->
-		{JPx, JPy} = lists:nth(1, JumpPoint),
-		Jump = obtenerJumpNodo(ListaGlobal, {JPx, JPy}),
-		if Jump == [] ->
-			JumpNodo = [{JPx, JPy}, 0, 0, 0, 0, {X, Y}];
+%Recorre la lista de vecinos, similar al de A*
+jp_algorithm_proc_vecinos([Vecino | RestoVecinos], Global, Nodo =[{X, Y}, G , _], Open, {Fx, Fy}, NodosVisitados) -> 
+	board ! {movejp, Vecino},
+	NuevosVisitados = lists:append(NodosVisitados, [Vecino]),						
+	Salto = saltar(Vecino, {X, Y}, {Fx, Fy}), %Verifica si se puede saltar o no a esa posición
+	if Salto /= [] ->
+		{SaltoX, SaltoY} = lists:nth(1, Salto),
+		PuntoSalto = obtenerPuntoSalto(Global, {SaltoX, SaltoY}),
+		if PuntoSalto == [] ->
+			NodoSalto = [{SaltoX, SaltoY}, 0, 0, 0, 0, {X, Y}];
 		true ->
-			JumpNodo = Jump
+			NodoSalto = PuntoSalto
 		end,
-		Closed = isclosed({JPx, JPy}, ListaGlobal),
-		if Closed ->
-				foreachVecino(T, ListaGlobal, Nodo, OpenList, {Fx, Fy}, VisitadosN);
+		EstaClosed = estaClosed({SaltoX, SaltoX}, Global),
+		if EstaClosed ->
+				jp_algorithm_proc_vecinos(RestoVecinos, Global, Nodo, Open, {Fx, Fy}, NodosVisitados);
 		true -> 
-				D = euclides({X, Y},{JPx, JPy}),
-				NG = G + D,
-				Open = isopened({JPx, JPy}, ListaGlobal),
-				JPG = lists:nth(2, JumpNodo),
-				if (not Open) or (NG < JPG) ->
-						if (not Open) ->
-							Heuristica = euclides( {JPx, JPy}, {Fx, Fy} ),
-							JumpNodoN_Open_OL = [{JPx, JPy}, NG, Heuristica],
-							JumpNodoN_Open_GL = [{JPx, JPy}, NG, Heuristica, 1, lists:nth(5, JumpNodo), {X, Y}],
-							OpenListN = lists:append(OpenList, [JumpNodoN_Open_OL]),
-							ListaGlobalN = lists:append(ListaGlobal, [JumpNodoN_Open_GL]),
-							board ! {movejp, {JPx, JPy}},
-							VisitadosNN = lists:append(VisitadosN, [{JPx, JPy}]),
-							[OpenListN, ListaGlobalN, VisitadosNN];
-							%foreachVecino(T, ListaGlobalN, Nodo, OpenListN, {Fx, Fy}, VisitadosNN);
+				Distancia = euclides({X, Y},{SaltoX, SaltoY}),
+				NuevoG = G + Distancia,
+				EstaOpen = estaOpen({SaltoX, SaltoY}, Global),
+				ValorGPuntoSalto = lists:nth(2, NodoSalto),
+				if (not EstaOpen) or (NuevoG < ValorGPuntoSalto) ->
+						if (not EstaOpen) ->
+							NuevoSaltoNodoL = [{SaltoX, SaltoY}, NuevoG, 
+								euclides( {SaltoX, SaltoY}, {Fx, Fy} )],
+
+							NuevoSaltoNodoG = [{SaltoX, SaltoY}, NuevoG, euclides( {SaltoX, SaltoY}, {Fx, Fy} ), 1, lists:nth(5, NodoSalto), {X, Y}],
+							NuevaOpen = lists:append(Open, [NuevoSaltoNodoL]),
+							NuevaGlobal = lists:append(Global, [NuevoSaltoNodoG]),
+							board ! {movejp, {SaltoX, SaltoY}},
+							[NuevaOpen, NuevaGlobal, lists:append(NuevosVisitados, [{SaltoX, SaltoY}])];
 						true ->
-							foreachVecino(T, ListaGlobal, Nodo, OpenList, {Fx, Fy}, VisitadosN)
+							jp_algorithm_proc_vecinos(RestoVecinos, Global, Nodo, 
+								Open, {Fx, Fy}, NodosVisitados)
 						end;
 				true->
-						ListaGlobalN = lists:append(ListaGlobal, [JPG]),
-						foreachVecino(T, ListaGlobalN, Nodo, OpenList, {Fx, Fy}, VisitadosN)
+						NuevaGlobal = lists:append(Global, [ValorGPuntoSalto]),
+						jp_algorithm_proc_vecinos(RestoVecinos, NuevaGlobal, 
+							Nodo, Open, {Fx, Fy}, NodosVisitados)
 				end
 		end;
 	true ->
-			foreachVecino(T, ListaGlobal, Nodo, OpenList, {Fx, Fy}, VisitadosN)
+			jp_algorithm_proc_vecinos(RestoVecinos, Global, 
+							Nodo, Open, {Fx, Fy}, NodosVisitados)
 	end;
-foreachVecino([], ListaGlobal, _, OpenList, _, Visitados) -> [OpenList, ListaGlobal, Visitados].
+jp_algorithm_proc_vecinos([], Global, _, Open, _, NodosVisitados) -> [Open, Global, NodosVisitados].
 
-
-estaPadre(_, []) -> false;
-estaPadre(Coord, [H | T]) ->
-	Temp = lists:nth(6, H),
-	if Temp == Coord ->
-		true;
-	true->
-		estaPadre(Coord, T)
-	end.
-
-
-jump(Vecino = {X, Y}, {PX, PY}, NodoFinal)->
-	DX = X - PX,
-	DY = Y - PY,
-	W0 = is_walkable(X, Y),		
-	if (not W0) ->
-				[];
-	Vecino == NodoFinal ->
-				[{X, Y}];
-	%Checkeo de vecinos forzados en las diagonales
-	(DX /= 0) and (DY /= 0) ->
-				W1 = is_walkable((X - DX), (Y - DY)),
-				W2 = is_walkable((X - DX), Y),
-				W3 = is_walkable((X + DX), (Y - DY)),
-				W4 = is_walkable(X , (Y - DY)),
-				if (W1 and (not W2)) or (W3 and (not W4)) ->
-								[{X, Y}];
-				true ->
-								jump_aux(Vecino, {PX, PY}, NodoFinal)
-				end;
-	%Sino, diagonales y verticales
-	not ((DX /= 0) and (DY /= 0)) ->
-				%Moverse sobre el eje X
-				if (DX /= 0) ->
-						W5 = is_walkable((X + DX), (Y + 1)),
-						W6 = is_walkable(X, (Y + 1)),
-						W7 = is_walkable((X + DX), (Y - 1)),
-						W8 = is_walkable(X, (Y - 1)),
-						if (W5 and (not W6)) or (W7 and (not W8)) ->
-										[{X, Y}];
-						true ->
-										jump_aux(Vecino, {PX, PY}, NodoFinal)
-						end;
-				true ->
-						W9  = is_walkable((X + 1), (Y + DY)),
-						W10 = is_walkable((X + 1), Y),
-						W11 = is_walkable((X - 1), (Y + DY)),
-						W12 = is_walkable((X - 1), Y),
-						if (W9 and (not W10)) or (W11 and (not W12)) ->
-										[{X, Y}];
-						true ->
-										jump_aux(Vecino, {PX, PY}, NodoFinal)
-						end
-				end
-	end.
-
-jump_aux({X, Y}, {PX, PY}, {Ex, Ey}) ->
-	DX = X - PX,
-	DY = Y - PY,
-	if (DX /= 0) and (DY /= 0) ->
-				JX = jump({(X + DX), Y}, {X, Y}, {Ex, Ey}),
-				JY = jump({X, (Y + DY)}, {X, Y}, {Ex, Ey}),
-				if (JX /= []) or (JY /= []) ->
-						[{X, Y}];
-				true ->
-						jump_aux_2({X, Y}, {PX, PY}, {Ex, Ey})
-				end;
-	true ->
-				jump_aux_2({X, Y}, {PX, PY}, {Ex, Ey})	
-	end.
-
-jump_aux_2({X, Y}, {PX, PY}, {Ex, Ey}) ->
-	DX = X - PX,
-	DY = Y - PY,				
-	W13 = is_walkable((X + DX), Y),
-	W14 = is_walkable(X, (Y + DY)),
-	if (W13 or W14) ->
-				jump({(X + DX), (Y + DY)}, {X, Y}, {Ex, Ey});
-	true ->
-				[]
-	end.
-
-
-filtrarVecinos([], _, Retorno) -> Retorno;
-filtrarVecinos([H | T], Visitados, Retorno) ->
-	Esta = esta(H, Visitados),			
-	if Esta ->
-			filtrarVecinos(T, Visitados, Retorno);
-	true->
-			NRetorno = lists:append(Retorno, [H]),
-			filtrarVecinos(T, Visitados, NRetorno)
-	end.					
-
-
-%Averigua si una posicion en el board NO es una pared
-is_walkable(X, Y) ->
-	board ! {get_all_board, self()},
-            receive
-                Board -> Board end,
-    Pos = board:calcPos(X, Y),
-    if
-    Pos == invalid -> false;
-    true ->
-        case element(Pos, Board) of
-        ?WALL ->
-            false;
-        _ ->
-            true
-        end
-    end.
-
-%Obtiene un Nodo de la ListaGlobal, pasandole las coordenadas
-obtenerJumpNodo([], _) -> [];
-obtenerJumpNodo([H | T], Nodo) -> 
-	Coord = lists:nth(1, H),
-	if Coord == Nodo -> 
-			H;
-	true ->
-		obtenerJumpNodo(T, Nodo)
-	end.
-
-%Busca el estado opened de un Nodo en la ListaGlobal
-isopened(_, []) -> false;
-isopened(Nodo, [H|T]) ->
-	NodoTemp = lists:nth(1, H),
-	if Nodo == NodoTemp -> 
-		X = lists:nth(4, H),
-		if X == 1 -> 
-			true;
-		true -> 
-			false
-		end;
-	true ->
-		isopened(Nodo, T) 
-	end.
-
-%Busca el estado closed de un Nodo en la ListaGlobal
-isclosed(_, []) -> false;
-isclosed(Nodo, [H|T]) ->
+%Se buscan estados closed en la lista
+estaClosed(_, []) -> false;
+estaClosed(Nodo, [H|T]) ->
 	NodoTemp = lists:nth(1, H),
 	if Nodo == NodoTemp -> 
 		X = lists:nth(5, H),
@@ -571,68 +481,123 @@ isclosed(Nodo, [H|T]) ->
 			false
 		end;
 	true ->
-		isclosed(Nodo, T) 
+		estaClosed(Nodo, T) 
 	end.
 
-%Marca el estado opened = 1 de un Nodo en la ListaGlobal
-openNodo(_, [], Retorno) -> Retorno;
-openNodo(Nodo, [H | T], Retorno) -> 
+%Se buscan estados open en la lista
+estaOpen(_, []) -> false;
+estaOpen(Nodo, [H|T]) ->
 	NodoTemp = lists:nth(1, H),
-	NodoG = lists:nth(2, H),
-	NodoH = lists:nth(3, H),
-	NodoClose = lists:nth(5, H),
-	Padre = lists:nth(6 , H),
 	if Nodo == NodoTemp -> 
-		lists:append(Retorno, [[Nodo, NodoG, NodoH, 1, NodoClose, Padre]]);
+		X = lists:nth(4, H),
+		if X == 1 -> 
+			true;
+		true -> 
+			false
+		end;
 	true ->
-		NRetorno = lists:append(Retorno, [H]),
-		openNodo(Nodo, T, NRetorno) 
-	end.	
-
-%Marca el estado closed = 1 de un Nodo en la ListaGlobal
-closeNodo(_, [], Retorno) -> Retorno;
-closeNodo(Nodo, [H | T], Retorno) -> 
-	NodoTemp = lists:nth(1, H),
-	NodoG = lists:nth(2, H),
-	NodoH = lists:nth(3, H),
-	NodoOpen = lists:nth(4, H),
-	Padre = lists:nth(6 , H),
-	if Nodo == NodoTemp -> 
-		lists:append(Retorno, [[Nodo, NodoG, NodoH, NodoOpen, 1, Padre]]);
-	true ->
-		NRetorno = lists:append(Retorno, [H]),
-		closeNodo(Nodo, T, NRetorno) 
+		estaOpen(Nodo, T) 
 	end.
-	
-%Llama al board a moverse a una posicion
-moverse(Nodo) ->
-	board ! {my_move, Nodo}.
-	
-%Obtiene la posicion actual del board para guardarla
-guardar() ->
-	board ! {get_pos, self()},
-	receive X->X end.
+
+%Obtiene un nuevo punto de salto de acuerdo a las coordenadas que se pasan por parámetro
+obtenerPuntoSalto([], _) -> [];
+obtenerPuntoSalto([H | T], Nodo) -> 
+	Coordenada = lists:nth(1, H),
+	if Coordenada == Nodo -> 
+			H;
+	true ->
+		obtenerPuntoSalto(T, Nodo)
+	end.
+
+%Verifica vertical,diagonal y horizontalmente para ver si el salto es válido y si hay vecinos forzados
+saltar(Vecino = {X, Y}, {SaltoX, SaltoY}, Final)->
+	DistanciaX = X - SaltoX,
+	DistanciaY = Y - SaltoY,
+	CuadroValido = posicionValida(X, Y),		
+	if (not CuadroValido) ->
+		[];
+	Vecino == Final ->
+		[{X, Y}];
+
+	%Verificación de horizontales y verticales
+	not ((DistanciaX /= 0) and (DistanciaY /= 0)) ->
+		%Horizontalmente
+		if (DistanciaX /= 0) ->
+			HorizontalVertical = posicionValida((X + DistanciaX), (Y + 1)),
+			Vertical = posicionValida(X, (Y + 1)),
+			HorizontalVerticalAux = posicionValida((X + DistanciaX), (Y - 1)),
+			VerticalAux = posicionValida(X, (Y - 1)),
+			if (HorizontalVertical and (not Vertical)) or (HorizontalVerticalAux and (not VerticalAux)) ->
+				[{X, Y}];
+			true ->
+				saltarAux(Vecino, {SaltoX, SaltoY}, Final)
+			end;
+		%Verticalmente
+		true ->
+			VerticalHorizontal  = posicionValida((X + 1), (Y + DistanciaY)),
+			Vertical = posicionValida((X + 1), Y),
+			VerticalHorizontalAux = posicionValida((X - 1), (Y + DistanciaY)),
+			VerticalAux = posicionValida((X - 1), Y),
+			if (VerticalHorizontal and (not Vertical)) or (VerticalHorizontalAux and (not VerticalAux)) ->
+				[{X, Y}];
+			true ->
+				saltarAux(Vecino, {SaltoX, SaltoY}, Final)
+			end
+		end;
+	%Diagonalmente
+	(DistanciaX /= 0) and (DistanciaY /= 0) ->
+		DiagonalNegativa = posicionValida((X - DistanciaX), (Y - DistanciaY)),
+		DiagonalHorizontal = posicionValida((X - DistanciaX), Y),
+		DiagonalPositiva = posicionValida((X + DistanciaX), (Y - DistanciaY)),
+		DiagonalVertical = posicionValida(X , (Y - DistanciaY)),
+		if (DiagonalNegativa and (not DiagonalHorizontal)) or (DiagonalPositiva and (not DiagonalVertical)) ->
+			[{X, Y}];
+		true ->
+			saltarAux(Vecino, {SaltoX, SaltoY}, Final)
+		end	
+	end.
+
+saltarAux({X, Y}, {SaltoX, SaltoY}, {FinalX, FinalY}) ->
+	DistanciaX = X - SaltoX,
+	DistanciaY = Y - SaltoY,
+	if (DistanciaX /= 0) and (DistanciaY /= 0) ->
+		NuevoX = saltar({(X + DistanciaX), Y}, {X, Y}, {FinalX, FinalY}),
+		NuevoY = saltar({X, (Y + DistanciaY)}, {X, Y}, {FinalX, FinalY}),
+		if (NuevoX /= []) or (NuevoY /= []) ->
+				[{X, Y}];
+		true ->
+				saltarDeNuevo({X, Y}, {SaltoX, SaltoY}, {FinalX, FinalY})
+		end;
+	true ->
+		saltarDeNuevo({X, Y}, {SaltoX, SaltoY}, {FinalX, FinalY})	
+	end.
+
+saltarDeNuevo({X, Y}, {SaltoX, SaltoY}, {FinalX, FinalY}) ->
+	DistanciaX = X - SaltoX,
+	DistanciaY = Y - SaltoY,				
+	Horizontal = posicionValida((X + DistanciaX), Y),
+	Vertical = posicionValida(X, (Y + DistanciaY)),
+	if (Horizontal or Vertical) ->
+		saltar({(X + DistanciaX), (Y + DistanciaY)}, {X, Y}, {FinalX, FinalY});
+	true ->
+		[]
+	end.				
 
 
-%Obtiene la cordenada del nodo con el menor F de la lista Open
-obtenerMinimo([]) -> [];
-obtenerMinimo(OpenList = [H | T]) -> 
-	Fs = obtenerFs([H | T], []),
-	Min = lists:min(Fs),
-	I = indiceMin(Fs, Min, 1),
-	lists:nth(I, OpenList).
-
-
-%Calcula el Fs de los nodos en una lista
-obtenerFs([], Final) -> Final;
-obtenerFs([H | T], Final) ->
-	F = (lists:nth(2, H) + lists:nth(3, H)),
-	NFinal = lists:append(Final, [F]),
-	obtenerFs(T, NFinal).	
-
-
-%Encuentra el indice de un numero X en una lista
-indiceMin([H|_], Min, I) when H == Min -> I;
-indiceMin([_|T], Min, I) -> 
-	IS = I + 1,
-	indiceMin(T, Min, IS).
+%Averigua si una posicion en el board NO es una pared
+posicionValida(X, Y) ->
+	board ! {get_all_board, self()},
+	receive
+	    Tablero ->
+			Posicion = board:calcPos(X, Y),
+				if Posicion == invalid -> 
+					false;
+			true ->
+	    		case element(Posicion, Tablero) of
+			        ?WALL ->
+			            false;
+			        _ ->
+			            true
+			    end
+			end
+    end.
